845a8703f12544c52f07677393e5ff5a9337991c
Victor B <haristolhogamer@gmail.com>
2025-11-05 19:35:57 -0300

    Add tree farm textures

--- src/main/java/com/victorbrndls/indus/Indus.java
+++ src/main/java/com/victorbrndls/indus/Indus.java
@@ -4,7 +4,10 @@ import com.victorbrndls.indus.blocks.IndusBlocks;
 import com.victorbrndls.indus.blocks.tileentity.IndusTileEntities;
 import com.victorbrndls.indus.items.IndusItems;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -13,6 +16,7 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,10 +28,18 @@ public class Indus {
 
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> INDUS_TAB =
+            CREATIVE_MODE_TABS.register("tab", () -> CreativeModeTab.builder()
+                    .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
+                    .title(Component.literal("Indus"))
+                    .icon(() -> new ItemStack(IndusItems.TREE_FARM.get()))
+                    .displayItems((parameters, output) -> {
+                        output.accept(new ItemStack(IndusItems.TREE_FARM.get()));
+                    }).build());
+
     public Indus(IEventBus eventBus, ModContainer modContainer) {
         eventBus.addListener(IndusTileEntities::onRegisterCapabilities);
 
