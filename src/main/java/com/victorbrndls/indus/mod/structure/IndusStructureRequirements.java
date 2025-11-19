21fd0c8f42b1907501a2c46125724b0316fa9770
Victor B <haristolhogamer@gmail.com>
2025-11-19 11:05:39 -0300

    Add recipes

--- src/main/java/com/victorbrndls/indus/mod/structure/IndusStructureRequirements.java
+++ src/main/java/com/victorbrndls/indus/mod/structure/IndusStructureRequirements.java
@@ -10,68 +10,69 @@ public class IndusStructureRequirements {
 
     public static List<ItemStack> getRequirements(IndusStructure structure) {
         return switch (structure) {
+            // available: concrete slab
             case TREE_FARM -> List.of(
-                    new ItemStack(IndusItems.CONCRETE_SLAB.get(), 32),
-                    new ItemStack(Items.OAK_LOG, 48),
-                    new ItemStack(Items.CRAFTING_TABLE, 6),
-                    new ItemStack(Items.STONECUTTER, 2),
-                    new ItemStack(Items.IRON_AXE, 1)
+                    new ItemStack(IndusItems.CONCRETE_SLAB.get(), 64),
+                    new ItemStack(Items.STONECUTTER, 8),
+                    new ItemStack(Items.OAK_SAPLING, 32),
+                    new ItemStack(Items.DIAMOND_AXE, 4)
             );
             case QUARRY -> List.of(
                     new ItemStack(IndusItems.CONCRETE_SLAB.get(), 96),
-                    new ItemStack(Items.STONE, 128),
-                    new ItemStack(Items.GRAVEL, 64),
-                    new ItemStack(Items.FURNACE, 8),
-                    new ItemStack(Items.IRON_PICKAXE, 1)
+                    new ItemStack(Items.HOPPER, 8),
+                    new ItemStack(Items.TORCH, 64),
+                    new ItemStack(Items.DIAMOND_PICKAXE, 4),
+                    new ItemStack(Items.DIAMOND_SHOVEL, 4)
             );
             case BLAST_FURNACE -> List.of(
                     new ItemStack(IndusItems.CONCRETE_SLAB.get(), 96),
                     new ItemStack(Items.BRICKS, 48),
-                    new ItemStack(Items.BLAST_FURNACE, 8),
-                    new ItemStack(Items.HOPPER, 8),
-                    new ItemStack(Items.COAL_BLOCK, 8)
+                    new ItemStack(Items.BLAST_FURNACE, 16),
+                    new ItemStack(Items.FURNACE, 16),
+                    new ItemStack(Items.HOPPER, 12)
             );
+            // available: construction part 1
             case MIXER -> List.of(
-                    new ItemStack(IndusItems.CONCRETE_SLAB.get(), 96),
+                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 32),
                     new ItemStack(Items.STONE_BRICKS, 48),
-                    new ItemStack(Items.CAULDRON, 8),
-                    new ItemStack(Items.HOPPER, 4),
-                    new ItemStack(Items.OBSIDIAN, 4)
+                    new ItemStack(Items.CAULDRON, 16),
+                    new ItemStack(Items.HOPPER, 12),
+                    new ItemStack(Items.CRAFTING_TABLE, 4)
             );
             case PUMP -> List.of(
-                    new ItemStack(IndusItems.CONCRETE_SLAB.get(), 96),
-                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 8),
-                    new ItemStack(Items.CAULDRON, 8),
-                    new ItemStack(Items.HOPPER, 4),
-                    new ItemStack(Items.PISTON, 4)
+                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 40),
+                    new ItemStack(Items.STONE_BRICKS, 48),
+                    new ItemStack(Items.CAULDRON, 16),
+                    new ItemStack(Items.HOPPER, 12),
+                    new ItemStack(Items.PISTON, 16)
             );
             case STEAM_GENERATOR -> List.of(
-                    new ItemStack(IndusItems.CONCRETE_SLAB.get(), 96),
-                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 8),
-                    new ItemStack(Items.COPPER_BLOCK, 16),
-                    new ItemStack(Items.HOPPER, 4),
-                    new ItemStack(Items.PISTON, 4)
+                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 64),
+                    new ItemStack(Items.COPPER_BLOCK, 32),
+                    new ItemStack(Items.CAULDRON, 16),
+                    new ItemStack(Items.HOPPER, 12),
+                    new ItemStack(Items.PISTON, 16)
             );
             case CRUSHER -> List.of(
-                    new ItemStack(IndusItems.CONCRETE_SLAB.get(), 96),
-                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 8),
-                    new ItemStack(Items.COPPER_BLOCK, 16),
-                    new ItemStack(Items.HOPPER, 4),
-                    new ItemStack(Items.PISTON, 8)
+                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 64),
+                    new ItemStack(Items.IRON_BLOCK, 32),
+                    new ItemStack(Items.GRINDSTONE, 16),
+                    new ItemStack(Items.HOPPER, 16),
+                    new ItemStack(Items.PISTON, 32)
             );
             case MAINTENANCE_DEPOT -> List.of(
-                    new ItemStack(IndusItems.CONCRETE_SLAB.get(), 96),
-                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 8),
+                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 96),
+                    new ItemStack(Items.IRON_BLOCK, 16),
                     new ItemStack(Items.COPPER_BLOCK, 16),
-                    new ItemStack(Items.HOPPER, 4),
-                    new ItemStack(Items.PISTON, 2)
+                    new ItemStack(Items.CRAFTING_TABLE, 16),
+                    new ItemStack(Items.PISTON, 32)
             );
             case ASSEMBLER_1 -> List.of(
-                    new ItemStack(IndusItems.CONCRETE_SLAB.get(), 96),
-                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 8),
-                    new ItemStack(Items.COPPER_BLOCK, 16),
-                    new ItemStack(Items.HOPPER, 4),
-                    new ItemStack(Items.PISTON, 1)
+                    new ItemStack(IndusItems.CONSTRUCTION_PART_1.get(), 96),
+                    new ItemStack(Items.IRON_BLOCK, 8),
+                    new ItemStack(Items.COPPER_BLOCK, 8),
+                    new ItemStack(Items.CRAFTING_TABLE, 32),
+                    new ItemStack(Items.CRAFTER, 40)
             );
         };
     }
