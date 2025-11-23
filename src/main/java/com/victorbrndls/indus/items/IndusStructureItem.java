6c49b54a5dccc058aa7d9d6940a19b668e44496d
Victor B <haristolhogamer@gmail.com>
2025-11-23 07:54:26 -0300

    Enhance tooltip information for IndusStructureItem

--- src/main/java/com/victorbrndls/indus/items/IndusStructureItem.java
+++ src/main/java/com/victorbrndls/indus/items/IndusStructureItem.java
@@ -2,11 +2,15 @@ package com.victorbrndls.indus.items;
 
 import com.victorbrndls.indus.mod.structure.IndusStructure;
 import com.victorbrndls.indus.mod.structure.IndusStructureHelper;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+
+import java.util.List;
 
 public class IndusStructureItem extends BlockItem {
 
@@ -32,5 +36,32 @@ public class IndusStructureItem extends BlockItem {
 
     public IndusStructure getStructure() {
         return structure;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+
+        var usesEnergy = false;
+        var usesMaintenance = false;
+
+        switch (structure) {
+            case TREE_FARM, QUARRY, BLAST_FURNACE, MIXER, STEAM_GENERATOR, MAINTENANCE_DEPOT -> {
+            }
+            case PUMP -> {
+                usesMaintenance = true;
+            }
+            case CRUSHER, ASSEMBLER_1 -> {
+                usesEnergy = true;
+                usesMaintenance = true;
+            }
+        }
+
+        if (usesEnergy) {
+            tooltipComponents.add(Component.literal("Consumes ").append(Component.literal("Energy").withColor(0xFF00FF00)));
+        }
+        if (usesMaintenance) {
+            tooltipComponents.add(Component.literal("Consumes ").append(Component.literal("Maintenance").withColor(0xFF00AAFF)));
+        }
     }
 }
